b5fb0dd7af89b46a9dc6a78757128fdac199c034
Adam Zethraeus <adam@zethrae.us>
2021-05-05 15:57:44 -0700

    inverted index

--- proj/EmojiThing/Shared/XMLTest.swift
+++ proj/EmojiThing/Shared/XMLTest.swift
@@ -8,6 +8,22 @@ class XMLTest {
         let xmlData = try! Data(contentsOf: fileURL!)
         let string = String(decoding: xmlData, as: UTF8.self)
         let xml = try! XML.parse(string)
-        print(xml)
+        let iterator = xml.ldml.annotations.annotation.makeIterator()
+        var termMap = [String: [String]]()
+        for annotation in iterator {
+            let emoji = annotation.attributes["cp"]!
+            let tags = annotation.text!.split(separator: "|").map { $0.trimmingCharacters(in: .whitespaces) }.flatMap { $0.split(separator: " ").map{ String($0) }
+            }
+            termMap[emoji] = (termMap[emoji] ?? []) + tags
+        }
+        var invertedMap = [String: [String]]()
+        for (key, val) in termMap {
+            let emoji = key
+            let vals = Set(val)
+            for val in vals {
+                invertedMap[val] = (invertedMap[val] ?? []) + [emoji]
+            }
+        }
+        print(invertedMap)
     }
 }
